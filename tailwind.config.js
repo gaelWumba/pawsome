4a9c08ff8bfd0b39e0175596c9b84cb2d5f78037
gaelWumba <gael.wumbabuatu@gmail.com>
2024-09-17 01:16:53 +0200

    style: tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        color: {
+          1: "#AEECE7",
+          2: "#245A64",
+          3: "#2D3E43",
+        },
+      },
+      fontFamily: {
+        sans: ["var(--font-sora)", ...fontFamily.sans],
+        code: "var(--font-code)",
+        grotesk: "var(--font-grotesk)",
+      },
+    },
+  },
+  plugins: [],
+};
